0350f6f3dfad9ff7dd605e74d53b88bb4e2172e5
trevorweihon <trevorweihong.poon@student.unimelb.edu.au>
2023-05-23 18:15:05 +1000

    added gamechecking

---
+++ src/matachi/mapeditor/GameChecking.java
@@ -0,0 +1,69 @@
+package matachi.mapeditor;
+
+import java.io.*;
+import java.util.*;
+
+
+public class GameChecking {
+    // checks if folder
+    public static boolean isFolder(String input) {
+        File file = new File(input);
+        return file.isDirectory();
+    }
+
+    // returns list of string of all the xml files in the directory
+    public static List<String> getXMLFiles(String folderPath) {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        List<String> xmlFiles = new ArrayList<>();
+
+        for (int i = 0; i < files.length; i++) {
+            String filename = files[i].getName();
+            if (filename.endsWith(".xml") & Character.isDigit(filename.charAt(0))) {
+                xmlFiles.add(filename);
+                System.out.println(filename);
+            }
+        }
+        return xmlFiles;
+    }
+
+    public static String checkDuplicates(List<String> mapList) {
+        int number = Integer.parseInt(mapList.get(0).split("\\D+")[0]);
+        Set<String> duplicateMaps = new HashSet<>();
+
+        for (int i = 1; i < mapList.size(); i ++) {
+            // gets number
+            int indexnumber = Integer.parseInt(mapList.get(i).split("\\D+")[0]);
+            if (number == indexnumber) {
+                // adds the previous dupe
+                if (duplicateMaps.size() < 1) {
+                    duplicateMaps.add(mapList.get(i-1));
+                }
+                // adds the dupe
+                duplicateMaps.add(mapList.get(i));
+
+            }
+            else {
+                number = Integer.parseInt(mapList.get(i).split("\\D+")[0]);
+            }
+        }
+
+        StringBuilder duplicates = new StringBuilder();
+        // sorts array first
+        List<String> duplicateString = new ArrayList<>(duplicateMaps);
+        Collections.sort(duplicateString);
+        for (String duplicate : duplicateString) {
+            if (duplicates.length() > 0) {
+                duplicates.append("; ");
+            }
+            duplicates.append(duplicate);
+        }
+
+        duplicates.toString();
+        return duplicates.toString();
+            
+    }
+
+
+
+}
--- src/matachi/mapeditor/Main.java
+++ src/matachi/mapeditor/Main.java
@@ -1,25 +1,82 @@
 package matachi.mapeditor;
 
 import matachi.mapeditor.editor.Controller;
+import pacman.utility.GameCallback;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		String input = new String();
+	static boolean isItAFolder;
+	static String log;
+	private static FileWriter fileWriter = null;
+	private static String logFilePath = "Log.txt";
 
+	public static void writeString(String str) {
+		try {
+			fileWriter.write(str);
+			fileWriter.write("\n");
+			fileWriter.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// write to log file
+		try {
+			fileWriter = new FileWriter(logFilePath);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+
+		// gets input
+		String input;
 		if (args.length > 0) {
 			input = args[0];
+			isItAFolder = GameChecking.isFolder(input);
 		}
+		// if input is empty is null
 		else {
 			input = "";
 		}
 
+		// if empty just runs editor with no map
 		if (input.equals("")) {
 			System.out.println("null");
 			new Controller();
 		}
+		// if folder, checks if its a valid folder
+		else if (isItAFolder) {
+			System.out.println("its a folder");
+			List<String> xmlFiles = GameChecking.getXMLFiles(input);
+			// if non-valid, write to log file
+			if (xmlFiles.size() == 0) {
+				log = "[Game " + input + " – no maps found]";
+				writeString(log);
+			}
+
+			// then need to sort string numerically
+			Collections.sort(xmlFiles);
+			if (xmlFiles.size() > 1) {
+				System.out.println(xmlFiles);
+				String duplicates = GameChecking.checkDuplicates(xmlFiles);
+				// if there are duplicates
+				if (!duplicates.isEmpty()) {
+					log = "[Game " + input + " – multiple maps at same level: " + duplicates + "]";
+					writeString(log);
+				}
+			}
+		}
+		// if its 1 file. starts edit mode of that map
 		else {
-			System.out.println("theres smth there");
+			System.out.println("its an xml file");
 			new Controller().loadFile(input);
 		}
 	}
 }
+
+
