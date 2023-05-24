cb69df521eb7e63170272985c21116fbce31807d
lanthian <lanthian@student.unimelb.edu.au>
2023-05-24 16:20:53 +1000

    Automove sorta working - refinement needed...

--- src/pacman/PathFinder.java
+++ src/pacman/PathFinder.java
@@ -6,39 +6,38 @@ import ch.aplu.jgamegrid.Location;
 import java.util.*;
 
 public class PathFinder {
-    private ArrayList<Item> targets;
-    public PathFinder(ArrayList<Item> targets) {
+    private ArrayList<Location> targets;
+    public PathFinder(ArrayList<Location> targets) {
         this.targets = targets;
     }
 
     public ArrayList<Location> findNextLoc(PacActor pacActor) {
         // Build a queue of possible pathways to acceptable outcome
-        Queue<ArrayList<Location>> queue = new PriorityQueue<ArrayList<Location>>();
+        Queue<ArrayList<Location>> queue = new LinkedList<ArrayList<Location>>();
         ArrayList<Location> visited = new ArrayList<>();
 
         for (Location loc : pacActor.getValidMoves(pacActor.getLocation())) {
-            queue.add(new ArrayList<Location>(Collections.singletonList(loc)));
+            queue.add(new ArrayList<Location>(Arrays.asList(loc)));
             visited.add(loc);
         }
 
         // If queue size == 0, no reachable targets
         while (queue.size() > 0) {
-            // Iterate through possible targets
-            for (Item target : targets) {
+            ArrayList<Location> path = queue.peek();
+            Location lastMove = path.get(path.size() - 1);
+            // safe assertion given empty Location ArrayLists are never added
+            assert(lastMove != null);
 
+            // Iterate through possible targets
+            for (Location target : targets) {
                 // Check if target found at location
-                if (target.getLocation().equals(queue.peek())) {
+                if (target.equals(lastMove)) {
                     // Valid path to target found
                     return queue.peek();
                 }
             }
 
             // Not exited, so queue up more locations from this path
-            ArrayList<Location> path = queue.peek();
-            Location lastMove = path.get(path.size() - 1);
-            // safe assertion given empty Location ArrayLists are never added
-            assert(lastMove != null);
-
             for (Location loc : pacActor.getValidMoves(lastMove)) {
                 // Make sure not to revisit tiles
                 if (visited.contains(loc)) continue;
@@ -52,8 +51,9 @@ public class PathFinder {
             // Drop first element now
             queue.remove();
         }
+        System.out.println("DEBUG: Really don't want to be seeing this");
 
-        // No available target found - return null
-        return null;
+        // No available target found - return empty arraylist
+        return new ArrayList<Location>();
     }
 }
