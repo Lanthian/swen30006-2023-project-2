fdfa6109ff6430cab7fe886e97c214df1e525e9f
lanthian <lanthian@student.unimelb.edu.au>
2023-05-26 23:14:14 +1000

    Report added and folders fixed(?)

--- pacman/src/constants/Constants.java
+++ pacman/src/constants/Constants.java
@@ -6,10 +6,12 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String SPRITE_PATH = "sprites/game/";
-    public static final String TILE_PATH = "sprites/editor/";
-    public static final String DEFAULT_PROPERTIES_PATH = "properties/test.properties";
+    private static final String PARENT_FOLDER = "pacman/";
 
-    public final static String GAME_CHECKING_LOG_PATH = "GameCheckingLog.txt";
-    public final static String LOGS_FOLDER = "Game Folder/";
+    public static final String SPRITE_PATH = PARENT_FOLDER+"sprites/game/";
+    public static final String TILE_PATH = PARENT_FOLDER+"sprites/editor/";
+    public static final String DEFAULT_PROPERTIES_PATH = PARENT_FOLDER+"properties/test.properties";
+
+    public final static String GAME_CHECKING_LOG_PATH = PARENT_FOLDER+"GameCheckingLog.txt";
+    public final static String LOGS_FOLDER = PARENT_FOLDER+"Game Folder/";
 }
